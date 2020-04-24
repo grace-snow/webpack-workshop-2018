a53a33912153843d2b6199f0deced6f9da6beea7
grace.snow <graceaesnow@gmail.com>
2020-04-24 11:16:54 +0100

    update config with env, plugins. Notes in 02

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  mode: "none"
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => {
+  return {
+    mode: env.mode,
+    output: {
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.ProgressPlugin(),
+    ]
+  }
 };
